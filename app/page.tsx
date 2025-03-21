128534baf3775021fe87216af02646740a7080b5
dorea <dodomokhtari@hotmail.fr>
2025-03-21 15:35:39 +0100

    final

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ import Contact from '@/components/Contact';
 import Footer from '@/components/Footer';
 import Experience from '@/components/Experience';
 import Avenir from '@/components/Avenir';
-import './globals.css';
 
 
 export default function Home() {
